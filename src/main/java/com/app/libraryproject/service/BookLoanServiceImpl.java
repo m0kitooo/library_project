e56c5723e08563bd945973e379e6801beac4a3c2
Mikołaj Klimuszko <mikolajklimuszko123@gmail.com>
2025-06-03 23:14:41 +0200

    abc

--- src/main/java/com/app/libraryproject/service/BookLoanServiceImpl.java
+++ src/main/java/com/app/libraryproject/service/BookLoanServiceImpl.java
@@ -1,6 +1,5 @@
 package com.app.libraryproject.service;
 
-import com.app.libraryproject.dto.CreateBookRequest;
 import com.app.libraryproject.entity.BookLoan;
 import com.app.libraryproject.repository.BookLoanRepository;
 import lombok.AllArgsConstructor;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class BookLoanServiceImpl implements BookLoanService {
     private final BookLoanRepository bookLoanRepository;
-    private final BookLoanService bookLoanService;
     private final LibraryCardService libraryCardService;
 
     public void loanBook(Long id) {
