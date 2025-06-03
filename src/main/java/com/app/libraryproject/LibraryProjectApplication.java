b6a2fafe0561740e486bb94a8c2ba84c2ec2890f
katanima <133254760+katanima@users.noreply.github.com>
2025-06-03 19:26:08 +0200

    Update LibraryProjectApplication.java

--- src/main/java/com/app/libraryproject/LibraryProjectApplication.java
+++ src/main/java/com/app/libraryproject/LibraryProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryProjectApplication { //dsadsgfdssedg
+public class LibraryProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryProjectApplication.class, args);
