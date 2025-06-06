4fcf638653566aca667a3762540d012ab34407d2
Mikołaj Klimuszko <mikolajklimuszko123@gmail.com>
2025-06-06 19:14:02 +0200

    sigma commit

---
+++ src/main/java/com/app/libraryproject/config/exception/AppExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.app.libraryproject.config.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AppExceptionHandler {
+}
