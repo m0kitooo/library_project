f80c7d1956c64a1ce8404d68ca7fb854843a8ba5
Mikołaj Klimuszko <mikolajklimuszko123@gmail.com>
2025-06-06 19:17:15 +0200

    sigma commit

---
+++ src/main/java/com/app/libraryproject/entity/User.java
@@ -0,0 +1,25 @@
+package com.app.libraryproject.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private LocalDate birthday;
+}
