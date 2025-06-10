de81ca9869af620323ea31961428a29c777e2553
Mikołaj Klimuszko <mikolajklimuszko123@gmail.com>
2025-06-10 10:33:24 +0200

    nothing important

--- src/main/java/com/app/libraryproject/repository/BookLoanRepository.java
+++ src/main/java/com/app/libraryproject/repository/BookLoanRepository.java
@@ -4,4 +4,5 @@ import com.app.libraryproject.entity.BookLoan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookLoanRepository extends JpaRepository<BookLoan, Long> {
+
 }
