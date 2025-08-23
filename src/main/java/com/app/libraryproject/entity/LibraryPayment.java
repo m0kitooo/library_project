cd862478cfd7b75ba8f9d487456ce81b3841b5e3
m0kitooo <mikolajklimuszko123@gmail.com>
2025-08-23 17:57:18 +0200

    xpp

--- src/main/java/com/app/libraryproject/entity/LibraryPayment.java
+++ src/main/java/com/app/libraryproject/entity/LibraryPayment.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,11 +18,23 @@ public class LibraryPayment {
     @GeneratedValue
     private Long id;
 
+    @Column(name = "vendor", nullable = false, length = 100)
+    private String vendor;
+
+    @Column(name = "transaction_date", nullable = false)
+    private LocalDate transactionDate;
+
     @Column(name = "transaction_name", nullable = false)
     private String transactionName;
 
     @Column(nullable = false)
     private BigDecimal cost;
+
+    @Column(nullable = false, length = 10)
+    private String nip;
+
+    @Column(name = "invoice_number", length = 50)
+    private String invoiceNumber;
 
     @Lob
     private String description;
